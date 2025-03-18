a2d2352ac542ba70d6c0f3e7431256f6b3cc99fc
Lu Vieira <henrique2.terceiro@gmail.com>
2025-03-18 20:03:00 -0300

    Classe funcionario, seus atributos e metodos criados

--- src/main/java/org/projetoFuncionario/entity/Funcionario.java
+++ src/main/java/org/projetoFuncionario/entity/Funcionario.java
@@ -1,4 +1,18 @@
 package org.projetoFuncionario.entity;
 
 public class Funcionario {
+    private String nome;
+    private double horasTrabalhadas;
+    private double valorPorHora;
+
+    public Funcionario(String nome, float horasTrabalhadas, double valorPorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorPorHora = valorPorHora;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " horas trabalhadas: " + horasTrabalhadas + " Valor por hora: R$:valorPorHora";
+    }
 }
